3a8cde6e67a10470474d500097726130aa1c7cc4
Haysten Dcosta <haystendc@gmail.com>
2023-08-18 23:12:25 +0530

    Series Programs added....

--- Experiment02/Program14.sh
+++ Experiment02/Program14.sh
@@ -1,3 +1,36 @@
 # PROGRAM 14 : Write shell script to generate following series : 20, 22, 18, 20, 16, 18, 14, 16....
 # Programmer : Haysten D'costa
 #!/bin/bash
+
+i=1
+num=20
+echo "Enter the no. of terms : " # reading the no. of terms in series....
+read n
+while [ $i -le `expr $n` ]
+do
+    printf $num" " # displaying the term....
+    if [ `expr $i % 2` -ne 0 ]
+    then
+        num=`expr $num + 2`
+    else
+        num=`expr $num - 4`
+    fi
+    i=`expr $i + 1` # to increment i....
+done
+
+: ' # OR
+
+term_e=22
+term_o=20
+for(( i=1; i<=n; i++ ))
+do
+    if [ `expr $i % 2` -ne 0 ]
+    then
+        printf $term_o" "
+        term_o=`expr $term_o - 2`
+    else
+        printf $term_e" "
+        term_e=`expr $term_e - 2`
+    fi
+done
+'
--- Experiment02/Program16.sh
+++ Experiment02/Program16.sh
@@ -1,3 +1,20 @@
 # PROGRAM 17 : Generate the series : 1, 3, 2, 4, 3, 5, 4, 6....
 # Programmer : Haysten D'costa
 #!/bin/bash
+
+term_o=1
+term_e=3
+
+echo "Enter no. of terms in series : "; read n
+echo "Series is : "
+for(( i=1; i<=n; i++ ))
+do
+    if [ `expr $i % 2` -ne 0 ]
+    then
+        printf $term_o" "
+        term_o=`expr $term_o + 1`
+    else 
+        printf $term_e" "
+        term_e=`expr $term_e + 1`
+    fi
+done
