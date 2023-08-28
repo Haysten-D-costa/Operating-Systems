3cd9b5784544824eee6ae820cc88dbf7088e91fd
Haysten Dcosta <haystendc@gmail.com>
2023-08-28 19:36:38 +0530

    gitignore file added

--- Experiment02/Program15.sh
+++ Experiment02/Program15.sh
@@ -2,33 +2,27 @@
 # Programmer : Haysten D'costa
 
 #!/bin/bash 
-# The shebang specifies that this script should be interpreted by the Bash shell....
+# The 'shebang' specifies that this script should be interpreted by the Bash shell....
 
 echo -n "Enter the file name : "
 read file
 
 if [ -f "$file" ] # check if the file specified by the user exists in the 'current directory'....
 then
-    echo
-    echo -n "$file exist " # print a message indicating that the specified file exists....
+    echo -ne "\n$file exist " # print a message indicating that the specified file exists....
     
     if [ -w "$file" ] # check if the specified file has write permission....
     then
-        echo "and has write permissions"
+        echo -e "and has write permissions\n"
         
-        echo
-        echo "Enter Contents to add to file : "
-
-        cat << EOF
-
+        echo -e "Enter Contents to add to file : "        
         read contents
         echo $contents >> $file # Append the contents to the file using 'echo'....
         
-        echo
-        echo "Contents added : "
+        echo -e "\nContents added : "
         cat $file # Display the contents of the file using 'cat'....
     else
-        echo " but does not have write permissions"
+        echo -e " but does not have write permissions\n"
     fi
 else
     echo "File Doesn't Exist"
