8a7206b4cd9b027da907dc45696d2c771616e489
Haysten Dcosta <haystendc@gmail.com>
2023-11-07 19:41:31 +0530

    Experiment 07 Updated

--- Experiment07/DynamicStorageAllocationProblem.cpp
+++ Experiment07/DynamicStorageAllocationProblem.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 int block_count = 4;
 int process_count = 4;
-int blockSize[MAX] = {200, 100, 150, 60}, processSize[MAX] = {80, 200, 50, 120};
+int blockSize[MAX];
+int processSize[MAX];
 
 void sortAsc(int A[MAX], int n) {
     for(int i=0; i<n-1; i++) {
@@ -31,35 +32,79 @@ void sortDesc(int A[MAX], int n) {
         }
     }
 }
-void worstFit() {
-    
+
+void bestFit() 
+{
+    int j=0;
+    sortAsc(blockSize, block_count);
+    sortAsc(processSize, process_count);
+
     for(int i=0; i<block_count; i++) {
-        if(processSize[i] <= blockSize[i]) { cout << "P" << i << "(" << processSize[i] << ") : " << "B" << i << "(" << blockSize[i] << ")\n"; }
-        else { cout << "Unallocated !\n"; }
+        if(processSize[i] <= blockSize[i]) { 
+            cout << "P" << i+1 << "(" << processSize[i] << ") : " << "B" << i+1 << "(" << blockSize[j] << ")\n"; 
+            j++;
+        }
+        else { cout << "P" << i+1 << "(" << processSize[i] << ") :  Unallocated !\n"; }
     }
 }
-// void bestFit() {}
-// void firstFit() {}
+void firstFit() {
+    bool blockAllocated[block_count];
+    for(int i=0; i<block_count; i++) { blockAllocated[i] = false; }
+
+    for(int p=0; p<process_count; p++) {
+        int b = 0;
+        int flag = 0;
+        while(b < block_count) {
+            if(processSize[p] <= blockSize[b] && !blockAllocated[b]) {
+                cout << "P" << p+1 << "(" << processSize[p] << ") : " << "B" << b+1 << "(" << blockSize[b] << ")\n"; 
+                blockAllocated[b] = true;
+                flag = 1;
+                break;
+            }
+            b++;
+        }
+        if(flag == 0) { cout << "P" << p+1 << "(" << processSize[p] << ") :  Unallocated !\n"; }
+    }
+}
+void worstFit() {
+    int j = 0;
+    sortDesc(blockSize, block_count);
+
+    for(int i=0; i<block_count; i++) {
+        if(processSize[i] <= blockSize[i]) { 
+            cout << "P" << i+1 << "(" << processSize[i] << ") : " << "B" << i+1 << "(" << blockSize[j] << ")\n"; 
+            j++;
+        }
+        else { cout << "P" << i+1 << "(" << processSize[i] << ") :  Unallocated !\n"; }
+    }
+}
 
 int main() {
-    // cout << "\nEnter the no. of blocks : "; cin >> block_count;
-    // cout << "Enter the block sizes : \n";
-    // for(int i=0; i<block_count; i++) {
-    //     cout << "Block " << i << " : ";
-    //     cin >> blockSize[i];
-    // }
-    // cout << "\nEnter the no. of processes : "; cin >> process_count;
-    // cout << "Enter the process sizes : \n";
-    // for(int i=0; i<process_count; i++) {
-    //     cout << "Process " << i << " : ";
-    //     cin >> processSize[i];
-    // }
+    cout << "\nEnter the no. of blocks : "; cin >> block_count;
+    cout << "Enter the block sizes : \n";
+    for(int i=0; i<block_count; i++) {
+        cout << "Block " << i+1 << " : ";
+        cin >> blockSize[i];
+    }
+    cout << "\nEnter the no. of processes : "; cin >> process_count;
+    cout << "Enter the process sizes : \n";
+    for(int i=0; i<process_count; i++) {
+        cout << "Process " << i+1 << " : ";
+        cin >> processSize[i];
+    }
+    int choice;
+    cout << "\n1 <- Best Fit Algorithm" 
+         << "\n2 <- First Fit Algorithm"
+         << "\n3 <- Worst Fit Algorithm";
+    cout << "\nchoice ? : ";
+    cin >> choice;
+    cout << "\n";
 
-
-
-    // bestFit();
-    // firstFit();
-    worstFit();
-
+    switch(choice) {
+        case 1 : bestFit(); break;
+        case 2 : firstFit(); break;
+        case 3 : worstFit(); break;
+        default : cout << "Invalid choice !";
+    }
     return 0;
 }
